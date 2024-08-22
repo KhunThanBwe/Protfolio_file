b7588383d0c287e8a4723b9942171d7df3aeec51
KhunThanBwe <khunpaygi@gmail.com>
2024-08-22 10:42:18 +0630

    html,css,js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings: ['Web Developer',"Django Junior", 'Flask Junior', 'Odoo Junior'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop:true,
+  });
+
